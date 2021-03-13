d7584a8f4803cf211306a5b3b969441ed036c82a
Satan <2777973423@qq.com>
2021-03-13 19:50:20 +0800

    change

---
+++ char_''.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+int main()
+{
+    using namespace std;
+    char shirt_size = 's';      //''不可变为“”，C++不允许这种不合理的做法
+    cout << shirt_size;
+}
---
+++ char_string.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+int main()
+{
+    using namespace std;
+    char dog[8] = {'1','2','3','4','5','\0'};            //not a string;
+    cout << dog[1];
+}
